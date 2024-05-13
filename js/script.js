362297dfd5b5b5f8e740d3186a5e97ed4d15f7b6
Naufal Duta <naufaldutam@gmail.com>
2024-05-14 00:37:45 +0700

    Complete web storage

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 const books = [];
 const RENDER_EVENT = 'render-book';
 
+const SAVED_EVENT = 'saved-book';
+const STORAGE_KEY = 'BOOK_APPS';
+
 document.addEventListener('DOMContentLoaded', function () {
     const submitForm = document.getElementById('form');
     submitForm.addEventListener('submit', function (event) {
         event.preventDefault();
         addBook();
     })
-})
+
+    if (isStorageExist()) {
+        loadDataFromStorage();
+    }
+});
 
 function addBook() {
     const titleBook = document.getElementById('title').value;
@@ -19,6 +26,7 @@ function addBook() {
     books.push(bookObject);
 
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function generateId() {
@@ -124,6 +132,7 @@ function checkButtonFromCompleted(bookID) {
 
     bookTarget.isCompleted = true;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function findBook(bookID) {
@@ -143,6 +152,7 @@ function removeBookFromCompleted(bookID) {
 
     books.splice(bookTarget, 1);
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function undoBookFromCompleted(bookID) {
@@ -152,14 +162,49 @@ function undoBookFromCompleted(bookID) {
 
     bookTarget.isCompleted = false;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function findBookIndex(bookID) {
     for (const index in books) {
-        if(books[index].id === bookID) {
+        if (books[index].id === bookID) {
             return index;
         }
     }
 
     return -1;
+}
+
+function saveData() {
+    if (isStorageExist()) {
+        const parsed = JSON.stringify(books);
+        localStorage.setItem(STORAGE_KEY, parsed);
+        document.dispatchEvent(new Event(SAVED_EVENT));
+    }
+}
+
+function isStorageExist() {
+    if (typeof (storage) === undefined) {
+        alert('Browser kamu tidak mendukung local storage');
+        return false;
+    }
+
+    return true;
+}
+
+document.addEventListener(SAVED_EVENT, function() {
+    console.log(localStorage.getItem(STORAGE_KEY));
+})
+
+function loadDataFromStorage() {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    let data = JSON.parse(serializedData);
+
+    if (data !== null) {
+        for (const book of data) {
+            books.push(book);
+        }
+    }
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
 }
